2af477abd03fcf42a166a510cc4a31a37728c73d
Carlos Garcia Gonzalez <carlosggz@gmail.com>
2024-09-07 01:26:41 +0200

    Event annotation async version

--- src/main/java/com/example/springbootevent/services/ListenerServiceOne.java
+++ src/main/java/com/example/springbootevent/services/ListenerServiceOne.java
@@ -3,19 +3,19 @@ package com.example.springbootevent.services;
 import com.example.springbootevent.models.CustomApplicationEvent;
 import com.example.springbootevent.models.ValueObject;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Slf4j
-public class ListenerServiceOne implements ApplicationListener<CustomApplicationEvent> {
+public class ListenerServiceOne {
 
     public void process(ValueObject valueObject) {
         log.info("Event processing completed for value {} on service one", valueObject);
     }
 
-    @Override
+    @EventListener
     public void onApplicationEvent(CustomApplicationEvent event) {
         log.info("Received event {} on service one and thread {}", event.getValue(), Thread.currentThread().getName());
         process(event.getValue());
--- src/main/java/com/example/springbootevent/services/ListenerServiceTwo.java
+++ src/main/java/com/example/springbootevent/services/ListenerServiceTwo.java
@@ -3,18 +3,18 @@ package com.example.springbootevent.services;
 import com.example.springbootevent.models.CustomApplicationEvent;
 import com.example.springbootevent.models.ValueObject;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class ListenerServiceTwo implements ApplicationListener<CustomApplicationEvent> {
+public class ListenerServiceTwo {
 
     public void process(ValueObject valueObject) {
         log.info("Event processing completed for value {} on service two", valueObject);
     }
 
-    @Override
+    @EventListener
     public void onApplicationEvent(CustomApplicationEvent event) {
         log.info("Received event {} on service two and thread {}", event.getValue(), Thread.currentThread().getName());
         process(event.getValue());
